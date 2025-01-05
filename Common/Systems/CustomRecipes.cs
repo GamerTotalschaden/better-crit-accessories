11a4a075fb514783fd759693cf53e02e0d4623e6
Andreas Baur <andreas.baur@uni-ulm.de>
2025-01-05 18:43:54 +0100

    feat: add custom shimmer recipies (#18)

---
+++ Common/Systems/CustomRecipes.cs
@@ -0,0 +1,23 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterCritAccessories.Common.Systems
+{
+    public class CustomRecipes : ModSystem
+    {
+        public override void SetStaticDefaults()
+        {
+            // add shimmer recipies
+
+            // Flesh Knuckles into Putrid Scent (and vice versa)
+            ItemID.Sets.ShimmerTransformToItem[ItemID.FleshKnuckles] = ItemID.PutridScent;
+            ItemID.Sets.ShimmerTransformToItem[ItemID.PutridScent] = ItemID.FleshKnuckles;
+
+            // Panic Necklace into Band Of Starpower (and vice versa)
+            ItemID.Sets.ShimmerTransformToItem[ItemID.PanicNecklace] = ItemID.BandofStarpower;
+            ItemID.Sets.ShimmerTransformToItem[ItemID.BandofStarpower] = ItemID.PanicNecklace;
+        }
+
+        // other recipies can be added here by overriding AddRecipes()
+    }
+}
--- obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
+++ obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f673036d80e49ae2fda43db83cde20eb6c5e8c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07424eb83129b714487bdf7ca82f0d766b6e6bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
