d0d94594d7de0763d48b3c13b3ecfb1836714ff0
Andreas Baur <andreas.baur@uni-ulm.de>
2025-01-25 17:19:12 +0100

    feat: add Penetrating Critical Stinger (#7)

---
+++ Content/Items/Accessories/PenetratingCriticalStinger.cs
@@ -0,0 +1,43 @@
+﻿using BetterCritAccessories.Common.Players;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace BetterCritAccessories.Content.Items.Accessories
+{
+    public class PenetratingCriticalStinger : ModItem
+    {
+        public static readonly float ARMOR_PEN_INCREASE = 7;
+        public static readonly float CRIT_CHANCE_INCREASE_PERCENT = 10;
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(ARMOR_PEN_INCREASE, CRIT_CHANCE_INCREASE_PERCENT);
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+
+            Item.value = Item.buyPrice(gold: 1);
+            Item.rare = ItemRarityID.Pink;
+
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetArmorPenetration(DamageClass.Generic) += ARMOR_PEN_INCREASE;
+            player.GetModPlayer<BeesStingerOnCritPlayer>().Active = true;
+            player.GetCritChance(DamageClass.Generic) += CRIT_CHANCE_INCREASE_PERCENT;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<CriticalStinger>();
+            recipe.AddIngredient(ItemID.EyeoftheGolem);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.Register();
+        }
+    }
+}
--- obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
+++ obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13c039a1f3de1acd90a7b013442dc0fe6826c61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d29259d5ea6ac5e37e9b166fa36b0d7d5b3f520")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
