0cf0355863b973dde6fed17e3af979efa78a7746
Andreas Baur <andreas.baur@uni-ulm.de>
2025-03-15 16:57:54 +0100

    feat: add Brute Force (#11)

--- obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
+++ obj/Debug/net8.0/BetterCritAccessories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75a56ca3bb6923fddeecd9db98fe28d8fa5525d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ffb4f07b4e7c595e35ba312ca898156524ccba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterCritAccessories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
